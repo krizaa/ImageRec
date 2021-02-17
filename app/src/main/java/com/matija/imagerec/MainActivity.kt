f80312ee172bc540e98dd44bac1d5f7206866599
Matija Križanec <matija.krizanec@tagitinc.com>
2021-02-17 16:30:32 +0100

    Initial commit

---
+++ app/src/main/java/com/matija/imagerec/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.matija.imagerec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
