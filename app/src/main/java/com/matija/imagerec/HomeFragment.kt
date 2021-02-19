6dd6ef7aec424e5146f440a6ee526ad4ad73df22
Matija Križanec <matija.krizanec@tagitinc.com>
2021-02-19 19:13:50 +0100

    added home screen

---
+++ app/src/main/java/com/matija/imagerec/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.matija.imagerec
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.matija.imagerec.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil.inflate<HomeFragmentBinding>(inflater, R.layout.home_fragment, container, false)
+        binding.viewmodel = viewModel
+        return binding.root
+    }
+}
